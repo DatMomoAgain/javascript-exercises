2159dfa94764eb116318dd184b723aea5dd8d066
Mohak Khetan <mmkhetan101@gmail.com>
2023-02-12 05:12:29 +0530

    reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let ans='';
+    for(let i=0; i<string.length; i++)
+    {
+        ans+= string[string.length-i-1];
+    }
+    return ans;
 };
 
 // Do not edit below this line
