894bb4eaf9dd493fe0d40d0637e379bbea678905
Mohak Khetan <mmkhetan101@gmail.com>
2023-02-12 18:07:28 +0530

    sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a<0 || b<0)
+    {
+        return "ERROR";
+    }
+
+    if(a>b)
+    {
+        a = a+b;
+        b = a-b;
+        a = a-b;
+    }
+
+    let sum=0;
+    for(let i=a; i<=b; i++)
+    {
+        sum +=i;
+    }
+
+    return sum;
 
 };
 
