a08d5279f383cfe5c2ac9b91701ffd1a528a35d9
Mohak Khetan <mmkhetan101@gmail.com>
2023-02-12 17:46:48 +0530

    removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    for(let i=0; i<remove.length; i++)
+    {
+        let index = check(array, remove[i]);
+        if(index>-1)
+        {
+            array.splice(index,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//check if element is in array and return index
+function check(arr, n)
+{
+    for(let i=0; i<arr.length; i++)
+    {
+        if(arr[i]===n)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
